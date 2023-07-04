52aadcfcc44e0e89708dd62f95fb9bb51103c610
persjess <jesse.persinger.hvac@gmail.com>
2023-07-04 15:30:34 -0400

    added functionality to app

--- workshop24/src/App.jsx
+++ workshop24/src/App.jsx
@@ -4,15 +4,28 @@ import './App.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppies)
+  const [featPupId, setFeatPupId] = useState(null)
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  //console.log(puppies)
   return (
     <>
       <div>
         {
           puppies.map((puppy) => {
-            return <p>{puppy.name}</p>
+            // console.log(featuredPup);
+            return <p onClick={() => { setFeatPupId(puppy.id) }} key={puppy.id}>{puppy.name}</p>;
           })
         }
+
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
       </div>
 
     </>
